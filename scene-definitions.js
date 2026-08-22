3191061d6ede99d113611ff3188a7dd8145fcda5
Štěpán Chrast <chraststepan@gmail.com>
2026-08-22 16:26:41 +0200

    Wire motion polish runtime

--- scene-definitions.js
+++ scene-definitions.js
@@ -98,6 +98,7 @@
 
   ensureStylesheet('root-landing.css', 'data-profile-root-landing-style');
   ensureStylesheet('intro-animation.css', 'data-profile-intro-style');
+  ensureStylesheet('motion-polish.css', 'data-profile-motion-polish-style');
   prepareRootLandingDom();
 
   document.body.dataset.rootLanding = initialRootLanding ? 'true' : 'false';
@@ -296,4 +297,5 @@
 
   ensureScript('root-landing.js', 'data-profile-root-landing');
   ensureScript('intro-animation.js', 'data-profile-intro-animation');
+  ensureScript('motion-polish.js', 'data-profile-motion-polish');
 })();

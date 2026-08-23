2857735fc54e10896dc69716a82eb5e3fc6bdce3
Štěpán Chrast <chraststepan@gmail.com>
2026-08-24 00:55:16 +0200

    Keep artifact internal layout styles under scene composer

--- artifact-scene-bindings.js
+++ artifact-scene-bindings.js
@@ -90,6 +90,7 @@
   };
 
   if (typeof document !== 'undefined') {
+    ensureStyle('artifact-scenes-layout.css', 'data-profile-artifact-layout-style');
     ensureScript('scene-composer.js', 'data-profile-scene-composer');
     ensureScript('artifact-scene-layout-compat.js', 'data-profile-artifact-layout-compat');
     ensureStyle('portfolio-refinements.css', 'data-profile-refinements-style');

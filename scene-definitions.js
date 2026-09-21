ca0a29e6af4c5430e3715860cf9d1755aaa042df
Štěpán Chrast <chraststepan@gmail.com>
2026-09-21 10:46:35 +0200

    Remove dormant glass and pill fallbacks

--- scene-definitions.js
+++ scene-definitions.js
@@ -46,7 +46,7 @@
     document.head.appendChild(guard);
   }
 
-  const releaseRevision = '20260921-dossier3';
+  const releaseRevision = '20260921-dossier4';
   const versionedResource = resource =>
     `${resource}${resource.includes('?') ? '&' : '?'}v=${releaseRevision}`;
 
